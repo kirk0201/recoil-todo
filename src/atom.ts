0be1edb7ec30592f290c1fdcb5318a3d81645f4c
JaeHyen Kim <mine6546@gmail.com>
2022-04-14 12:40:51 +0900

    Fix: LocalStorage 배포 버그 Fix

--- src/atom.ts
+++ src/atom.ts
@@ -10,19 +10,14 @@ export const LOCAL_RECOIL_TODO = "LOCAL_RECOIL_TODO";
 export const LOCAL_CATEGORY = "LOCAL_CATEGORY";
 
 const getCategoryStorageHandler = localStorage.getItem(LOCAL_CATEGORY) || "[]";
-export const parsedCategoryStoraget = JSON.parse(getCategoryStorageHandler);
+export const parsedCategoryStorage = JSON.parse(getCategoryStorageHandler);
 
 const getToDoStorageHandler = localStorage.getItem(LOCAL_RECOIL_TODO) || "[]";
-export const parsedCategoryStorage = () => {
-  if (getToDoStorageHandler === "[]") {
-    return `["TO_DO", "DOING", "DONE"]`;
-  }
-  return JSON.parse(getToDoStorageHandler);
-};
+const parsedToDotorage = JSON.parse(getToDoStorageHandler);
 
 export const createCategory = atom({
   key: "createCategory",
-  default: [...parsedCategoryStoraget],
+  default: [...parsedCategoryStorage],
 });
 
 export const categoryState = atom({
@@ -31,7 +26,7 @@ export const categoryState = atom({
 });
 export const toDoState = atom<IToDo[]>({
   key: "toDoState",
-  default: parsedCategoryStorage(),
+  default: parsedToDotorage,
 });
 
 export const toDoSelector = selector({
@@ -39,8 +34,7 @@ export const toDoSelector = selector({
   get: ({ get }) => {
     const category = get(categoryState);
     const toDos = get(toDoState);
-    console.log("toDos : ", toDos);
-    console.log("category : ", category);
-    return toDos?.filter((toDo) => toDo.category.includes(category));
+
+    return toDos.filter((toDo) => toDo.category.includes(category));
   },
 });
